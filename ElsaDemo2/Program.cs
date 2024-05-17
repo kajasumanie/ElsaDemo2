f82ad12e0f8baef6b99e427891088d4699b8ec06
Kajasumanie Kanapathipillai <kajasumaniek@zone24x7.com>
2024-05-17 17:54:20 +0530

    dfdffd

--- ElsaDemo2/Program.cs
+++ ElsaDemo2/Program.cs
@@ -1,7 +1,14 @@
+using Elsa.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//ELSA
+builder.Services.AddElsaCore(options =>
+options.AddHttpActivities()).AddElsaApiEndpoints();
+
 
 var app = builder.Build();
 
@@ -12,6 +19,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//ELSA
+app.UseHttpActivities();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
